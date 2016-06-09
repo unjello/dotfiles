32dedfd7f1743d00d0f5c8ae87913be9c9760d8c
Andrzej Lichnerowicz <andrzej@lichnerowicz.pl>
2016-06-09 15:46:17 +0200

    [windows] add posh-git prompt

--- windows/ps/Microsoft.PowerShell_profile.ps1
+++ windows/ps/Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,16 @@
 Import-Module PSReadLine
 Set-PSReadlineOption -EditMode Emacs
 $env:path += ";" + $env:HOMEPATH + "\Documents\WindowsPowerShell\"
+
+# Load PoshGit
+Push-Location (Split-Path -Path $MyInvocation.MyCommand.Definition -Parent)
+Import-Module posh-git
+function global:prompt {
+    $realLASTEXITCODE = $LASTEXITCODE
+    Write-Host($pwd.ProviderPath) -nonewline
+    Write-VcsStatus
+    $global:LASTEXITCODE = $realLASTEXITCODE
+    return "> "
+}
+Pop-Location
+Start-SshAgent -Quiet
