de70d34b0bb8d4e6229e7d268176e5485d82e884
Andrzej Lichnerowicz <andrzej@lichnerowicz.pl>
2016-06-09 16:03:09 +0200

    [windows] add some git aliases from zsh

--- windows/ps/Microsoft.PowerShell_profile.ps1
+++ windows/ps/Microsoft.PowerShell_profile.ps1
@@ -14,3 +14,16 @@ function global:prompt {
 }
 Pop-Location
 Start-SshAgent -Quiet
+
+
+Set-Alias -Name which -Value Get-Command
+
+# gcm is by default bound to Get-Command, but I need it for Git
+Del alias:gcm -Force
+
+Function Unj-GitStatus { & git status }
+Function Unj-GitAdd { & git add $args }
+Function Unj-GitCommit { & git commit -m $args }
+Set-Alias -Name gws -Value Unj-GitStatus
+Set-Alias -Name gia -Value Unj-GitAdd
+Set-Alias -Name gcm -Value Unj-GitCommit
