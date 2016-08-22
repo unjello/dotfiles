fca32194e70513bd9e4fb1547611fb5f6ab6dada
Andrzej Lichnerowicz <andrzej@lichnerowicz.pl>
2016-08-22 11:17:21 +0200

    [windows] automate powershell and git installation

--- deploy_win.ps1
+++ deploy_win.ps1
@@ -1,2 +1,19 @@
+# http://martin.hoppenheit.info/blog/2015/using-powershell-behind-proxy/
+
+(New-Object System.Net.WebClient).Proxy.Credentials = [System.Net.CredentialCache]::DefaultNetworkCredentials
+
+Write-Host "Copying PowerShell Profile and user scripts"
 New-Item -ItemType Directory -Force -Path $env:HOMEDRIVE\$env:HOMEPATH\Documents\WindowsPowerShell | Out-Null
 Copy-Item windows\ps\*.ps1 $env:HOMEDRIVE\$env:HOMEPATH\Documents\WindowsPowerShell
+Write-Host
+
+Write-Host "Installing Chocolatey"
+iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
+Write-Host
+
+Write-Host "Installing applications from Chocolatey"
+cinst powershell -y
+cinst git -y
+cinst putty -y
+cinst poshgit -y
+Write-Host
