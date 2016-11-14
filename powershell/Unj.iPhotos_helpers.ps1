ae035728db0f38f23a9b0cdc2934c18a90cc36e5
Andrzej Lichnerowicz <andrzej@lichnerowicz.pl>
2016-11-15 00:15:48 +0100

    [powershell] add iPhotos helper
    iPhotos does not allow for easy mass extraction of photos. Source parameter need to point to Masters folder within iPhotos library.

---
+++ powershell/Unj.iPhotos_helpers.ps1
@@ -0,0 +1,11 @@
+﻿# Copyright (c) Andrzej Lichnerowicz. All rights reserved.
+# Licensed under the BSD License. See License.txt in the project root folder for license information.
+
+function Move-iPhotosMasters($Source, $Year, $Month, $Target) {
+    $src = Join-Path -Path $Source -ChildPath $Year -AdditionalChildPath $Month
+    $folder = "{0}.{1} -- Zrzut iPhone" -f $Year, $Month
+    $dst = Join-Path -Path $Target -ChildPath $Year -AdditionalChildPath $folder
+
+    New-Item -Path $dst -ItemType Directory -Force | Out-Null
+    Get-ChildItem -Path $src -Recurse -Include *.nef,*.mov,*.jpg,*.png,*.aae,*.jpeg,*.mp4 | % { Move-Item $_.FullName -Destination $dst }
+}
