fe27360898844baa7877e08dab59f64b70dd524a
Andrzej Lichnerowicz <andrzej@lichnerowicz.pl>
2018-06-12 17:26:37 +0200

    Cleanup macOS scripts

--- deploy_macos.sh
+++ deploy_macos.sh
@@ -4,13 +4,3 @@
 # Install Visual Studio Code (w/ Insiders) settings
 [[ -d $HOME/Library/Application\ Support/Code/User ]] && cp -f vscode/*.json $HOME/Library/Application\ Support/Code/User
 [[ -d $HOME/Library/Application\ Support/Code\ -\ Insiders/User ]] && cp -f vscode/*.json $HOME/Library/Application\ Support/Code\ -\ Insiders/User
-
-# Install Fira Code
-brew tap caskroom/fonts
-brew cask install font-fira-code font-fira-mono font-fira-mono-for-powerline
-
-# Install pip
-easy_install pip
-
-# Install [powerline-shell](https://github.com/powerline/powerline)
-pip install --user powerline-status
--- install_macos.sh
+++ install_macos.sh
@@ -1,3 +1,8 @@
 sudo easy_install pip
 pip install --user neovim
+# Install [powerline-shell](https://github.com/powerline/powerline)
 pip install --user powerline-status
+
+# Install Fira Code
+brew tap caskroom/fonts
+brew cask install font-fira-code font-fira-mono font-fira-mono-for-powerline
