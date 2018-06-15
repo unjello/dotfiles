c9a7ef00a8ad7c5e52297b3106d582d8cd0eae0a
Andrzej Lichnerowicz <andrzej@lichnerowicz.pl>
2018-06-15 17:37:42 +0200

    Add conan install for macOS

--- install_macos.sh
+++ install_macos.sh
@@ -6,3 +6,8 @@ pip install --user powerline-status
 # Install Fira Code
 brew tap caskroom/fonts
 brew cask install font-fira-code font-fira-mono font-fira-mono-for-powerline
+
+# Install conan and remote repositories
+brew install conan
+conan remote add bincrafters https://api.bintray.com/conan/bincrafters/public-conan
+conan remote add unjello https://api.bintray.com/conan/unjello/public 
