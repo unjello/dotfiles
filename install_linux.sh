6469b88a1e66042928640f779fbb4436e1b1754a
Andrzej Lichnerowicz <andrzej@lichnerowicz.pl>
2018-07-01 20:16:59 +0200

    Add git helpers installation

--- install_linux.sh
+++ install_linux.sh
@@ -3,3 +3,8 @@ pip install --user neovim
 
 # Install [powerline-shell](https://github.com/powerline/powerline)
 pip install --user powerline-status
+
+# Install git prompt helpers
+mkdir -p ~/bin
+curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash > ~/bin/.git-completion.bash
+curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh > ~/bin/.git-prompt.bash
--- install_macos.sh
+++ install_macos.sh
@@ -11,3 +11,8 @@ brew cask install font-fira-code font-fira-mono font-fira-mono-for-powerline
 brew install conan
 conan remote add bincrafters https://api.bintray.com/conan/bincrafters/public-conan
 conan remote add unjello https://api.bintray.com/conan/unjello/public 
+
+# Install git prompt helpers
+mkdir -p ~/bin
+curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash > ~/bin/.git-completion.bash
+curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh > ~/bin/.git-prompt.bash
