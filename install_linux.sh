68220a324fc22a0be37823164fdc8b770c8639ec
Andrzej Lichnerowicz <andrzej@lichnerowicz.pl>
2018-07-01 21:49:18 +0200

    Add git-ls installation

--- install_linux.sh
+++ install_linux.sh
@@ -8,3 +8,6 @@ pip install --user powerline-status
 mkdir -p ~/bin
 curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash > ~/bin/.git-completion.bash
 curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh > ~/bin/.git-prompt.bash
+
+# Install Git-ls
+pip install --user git+https://github.com/jonathf/gls.git
--- install_macos.sh
+++ install_macos.sh
@@ -16,3 +16,6 @@ conan remote add unjello https://api.bintray.com/conan/unjello/public
 mkdir -p ~/bin
 curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash > ~/bin/.git-completion.bash
 curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh > ~/bin/.git-prompt.bash
+
+# Install git-ls
+pip install --user git+https://github.com/jonathf/gls.git
