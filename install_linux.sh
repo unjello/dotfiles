5ddfbbbc3160326378abdb13e63ee2c3ab319826
Andrzej Lichnerowicz <andrzej@lichnerowicz.pl>
2018-07-02 18:27:44 +0200

    Move from gls to exa

--- install_linux.sh
+++ install_linux.sh
@@ -9,5 +9,10 @@ mkdir -p ~/bin
 curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash > ~/bin/.git-completion.bash
 curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh > ~/bin/.git-prompt.bash
 
-# Install Git-ls
-pip install --user git+https://github.com/unjello/gls.git@issue/1
+# Install exa
+mkdir -p ~/.tmp
+wget https://github.com/ogham/exa/releases/download/v0.8.0/exa-linux-x86_64-0.8.0.zip -O ~/.tmp/exa-linux.zip && \
+	cd ~/.tmp && \
+	unzip exa-linux.zip && \
+	sudo cp exa-linux-x86_64 /usr/local/bin && \
+	rm -rf ~/.tmp
--- install_macos.sh
+++ install_macos.sh
@@ -17,5 +17,5 @@ mkdir -p ~/bin
 curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash > ~/bin/.git-completion.bash
 curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh > ~/bin/.git-prompt.bash
 
-# Install git-ls
-pip install --user git+https://github.com/unjello/gls.git@issue/1
+# Install exa
+brew install exa
