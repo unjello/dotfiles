be25d7bf550555ba3e3c12a1fa1977f0b08bfc87
Andrzej Lichnerowicz <andrzej@lichnerowicz.pl>
2018-07-20 21:00:24 +0200

    Restore Roboto Mono as terminal font

--- deploy_linux.sh
+++ deploy_linux.sh
@@ -5,7 +5,7 @@
 [[ -d $HOME/.config/Code ]] && cp -f vscode/*.json $HOME/.config/Code/User/
 [[ -d "$HOME/.config/Code - Insiders" ]] && cp -f vscode/*.json "$HOME/.config/Code - Insiders/User/"
 
-gsettings set org.pantheon.terminal.settings font 'Fira Code 11'
+gsettings set org.pantheon.terminal.settings font 'Roboto Mono 11'
 
 # Ubuntu needs this file to be .bashrc, not profile like macOS
 ln -s $(pwd)/dot/bash_profile ~/.bashrc
