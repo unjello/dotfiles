cfd60b22b3209440371089394fc1164268107648
Andrzej Lichnerowicz <andrzej@lichnerowicz.pl>
2018-07-22 22:16:59 +0200

    Do vscode links for macOS too

--- deploy_macos.sh
+++ deploy_macos.sh
@@ -2,8 +2,10 @@
 # andrzej lichnerowicz, unlicensed (~public domain)
 
 # Install Visual Studio Code (w/ Insiders) settings
-[[ -d $HOME/Library/Application\ Support/Code/User ]] && cp -f vscode/*.json $HOME/Library/Application\ Support/Code/User
-[[ -d $HOME/Library/Application\ Support/Code\ -\ Insiders/User ]] && cp -f vscode/*.json $HOME/Library/Application\ Support/Code\ -\ Insiders/User
+[[ -d $HOME/.config/Code ]] && ln -s $(pwd)/vscode/keybindings.json $HOME/.config/Code/User/keybindings.json
+[[ -d $HOME/.config/Code ]] && ln -s $(pwd)/vscode/settings.json $HOME/.config/Code/User/settings.json
+[[ -d "$HOME/.config/Code - Insiders" ]] && ln -s $(pwd)/vscode/keybindings.json "$HOME/.config/Code - Insiders/User/keybindings.json"
+[[ -d "$HOME/.config/Code - Insiders" ]] && ln -s $(pwd)/vscode/settings.json "$HOME/.config/Code - Insiders/User/settings.json"
 
 # install bash_profile
 ln -s $(pwd)/dot/bash_profile ~/.bash_profile
