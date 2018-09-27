93c7f1f0dce628d3b1042f01e64f6ec111218bde
Andrzej Lichnerowicz <andrzej@lichnerowicz.pl>
2018-09-27 19:55:30 +0200

    deploy: Add OBS installation

--- install_linux.sh
+++ install_linux.sh
@@ -103,3 +103,9 @@ sudo usermod -aG docker $(whoami)
 sudo curl -L "https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chown root:docker /usr/local/bin/docker-compose
 sudo chmod ug+x /usr/local/bin/docker-compose
+
+# Install OBS
+sudo apt install ffmpeg -y
+sudo add-apt-repository ppa:obsproject/obs-studio -y
+sudo apt update -qq -y
+sudo apt install obs-studio -y
