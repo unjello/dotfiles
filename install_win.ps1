d90be8a5fc638ca0759aceb11a38097a9b887fb6
Andrzej Lichnerowicz <andrzej@lichnerowicz.pl>
2018-10-02 19:14:01 +0200

    deploy: add initial installs for Windows

--- install_win.ps1
+++ install_win.ps1
@@ -3,3 +3,9 @@ Invoke-Expression (New-Object Net.WebClient).DownloadString('https://get.scoop.s
 
 # install Chocolatey for tools not found in Scoop
 Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+scoop install git cmder-full
+scoop install openssh
+[environment]::setenvironmentvariable('GIT_SSH', (resolve-path (scoop which ssh)), 'USER')
+scoop bucket add extras
+scoop install extras/totalcommander
