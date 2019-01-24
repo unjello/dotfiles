393f973bbe9b2e00bd54b7aeb4e07a85e8bf83a9
Andrzej Lichnerowicz <andrzej@lichnerowicz.pl>
2019-01-24 06:33:00 -0600

    linux: Add Visual Studio Code installation

--- install_centos.sh
+++ install_centos.sh
@@ -9,3 +9,11 @@ sudo yum install -y python27-python-pip
 
 # Install pip3
 sudo yum install -y python36u python36u-pip python36u-devel
+
+# Install Visual Studio Code per
+# https://code.visualstudio.com/docs/setup/linux
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+
+sudo yum check-update
+sudo yum -y install code
