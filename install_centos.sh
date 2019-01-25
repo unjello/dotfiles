20240a2a497eede5280111bd29ceff56edf3747b
Andrzej Lichnerowicz <andrzej@lichnerowicz.pl>
2019-01-25 03:13:26 -0600

    install: Add installation of ripgrep for CentOS

--- install_centos.sh
+++ install_centos.sh
@@ -30,3 +30,9 @@ mkdir -p ~/bin
 curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash > ~/bin/.git-completion.bash
 curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh > ~/bin/.git-prompt.bash
 
+# Install ripgrep
+curl -LO https://github.com/BurntSushi/ripgrep/releases/download/0.10.0/ripgrep-0.10.0-x86_64-unknown-linux-musl.tar.gz
+tar -zxf ripgrep-0.10.0-x86_64-unknown-linux-musl.tar.gz
+cp ripgrep-0.10.0-x86_64-unknown-linux-musl.tar.gz/rg /usr/local/bin
+rm -rf ripgrep-0.10.0-x86_64-unknown-linux-musl*
+
