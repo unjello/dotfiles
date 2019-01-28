33644c4d4e66e66973e533f2628c4f3c841bdf20
Andrzej Lichnerowicz <andrzej@lichnerowicz.pl>
2019-01-28 07:18:22 -0600

    install: Add installation of cmake3

--- install_centos.sh
+++ install_centos.sh
@@ -36,8 +36,21 @@ tar -zxf ripgrep-0.10.0-x86_64-unknown-linux-musl.tar.gz
 cp ripgrep-0.10.0-x86_64-unknown-linux-musl/rg /usr/local/bin
 rm -rf ripgrep-0.10.0-x86_64-unknown-linux-musl*
 
+# Install both Cmake 2.x and 3.x
+sudo yum install -y cmake cmake3
+sudo alternatives --install /usr/local/bin/cmake cmake /usr/bin/cmake 10 \
+--slave /usr/local/bin/ctest ctest /usr/bin/ctest \
+--slave /usr/local/bin/cpack cpack /usr/bin/cpack \
+--slave /usr/local/bin/ccmake ccmake /usr/bin/ccmake \
+--family cmake
+sudo alternatives --install /usr/local/bin/cmake cmake /usr/bin/cmake3 20 \
+--slave /usr/local/bin/ctest ctest /usr/bin/ctest3 \
+--slave /usr/local/bin/cpack cpack /usr/bin/cpack3 \
+--slave /usr/local/bin/ccmake ccmake /usr/bin/ccmake3 \
+--family cmake
+
 # Install exa
-sudo yum install -y libgit2-devel cmake
+sudo yum install -y libgit2-devel
 sudo chown `whoami`: -R /usr/local
 git clone https://github.com/ogham/exa.git
 pushd exa; make install; popd
