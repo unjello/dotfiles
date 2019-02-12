7258becd4a41d198ab2757698d5591b47da1142c
Andrzej Lichnerowicz <andrzej@lichnerowicz.pl>
2019-02-12 02:25:34 -0600

    emacs: add initial init

---
+++ dot/emacs.d/init.el
@@ -0,0 +1,26 @@
+;; Code inspired by:
+;; https://github.com/gjstein/emacs.d/blob/master/init.el
+
+;; Install use-package if necessary
+(require 'package)
+(setq package-enable-at-startup nil)
+(setq package-archives (append package-archives
+			 '(("melpa" . "http://melpa.org/packages/")
+			 ("marmalade" . "http://marmalade-repo.org/packages/")
+			 ("gnu" . "http://elpa.gnu.org/packages/")
+			 ("org" . "http://orgmode.org/elpa/")
+			 ("elpy" . "http://jorgenschaefer.github.io/packages/"))))
+(package-initialize)
+
+;; Bootstrap `use-package'
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(use-package diminish :ensure t)
+
+;; Enable use-package
+(eval-when-compile
+  (require 'use-package))
+(require 'diminish)                ;; if you use :diminish
+(require 'bind-key)                ;; if you use any :bind variant
