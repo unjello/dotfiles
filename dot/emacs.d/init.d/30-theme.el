5a6ac933a6778c84cb6e10c6bce02c805779e6cb
Andrzej Lichnerowicz <andrzej@lichnerowicz.pl>
2019-02-13 02:11:47 -0600

    emacs: adjust UI

--- dot/emacs.d/init.d/30-theme.el
+++ dot/emacs.d/init.d/30-theme.el
@@ -1,3 +1,49 @@
 (require 'use-package)
 
+;; Excellent theme by Sarah Drasner @sdras
 (use-package night-owl-theme :ensure t)
+
+;; Set default fill column
+(setq-default fill-column 80)
+
+;; quiet, please! No dinging!
+(setq visible-bell nil)
+(setq ring-bell-function 'ignore)
+
+;; Disable menu bars, etc.
+(if window-system (scroll-bar-mode -1))
+(tool-bar-mode -1)
+(menu-bar-mode -1)
+
+;; No Backup Files
+(setq make-backup-files nil)
+
+;; Initial window
+(setq initial-frame-alist
+      '((width . 102)   ; characters in a line
+        (height . 54))) ; number of lines
+
+;; Subsequent frame
+(setq default-frame-alist
+      '((width . 100)   ; characters in a line
+(height . 52))) ; number of lines
+
+
+;; Diminish extraneous info in the modeline
+(diminish 'abbrev-mode)
+(defun sk/diminish-auto-revert ()
+  "Diminishes the 'auto-revert-mode' in the mode line."
+  (interactive)
+  (diminish 'auto-revert-mode ""))
+(add-hook 'auto-revert-mode-hook 'sk/diminish-auto-revert)
+
+(use-package powerline
+  :ensure t
+  :after spaceline-config
+  :config
+  (setq
+   powerline-height (truncate (* 1.0 (frame-char-height)))
+   powerline-default-separator 'utf-8)
+)
+~                                                                              
+~                                   
