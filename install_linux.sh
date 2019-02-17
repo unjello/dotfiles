01899079a7e910a200606704b4a2f4ab5b2acae1
Andrzej Lichnerowicz <andrzej@lichnerowicz.pl>
2019-02-17 22:34:55 +0100

    install: upgrade to node 11

--- install_linux.sh
+++ install_linux.sh
@@ -64,8 +64,8 @@ fc-cache -f
 mkdir -p ~/.nvm
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
 source ~/.nvm/nvm.sh
-nvm install 10
-nvm use 10
+nvm install 11
+nvm use 11
 npm i -g npm
 npm i -g pnpm
 npm i -g pm2
