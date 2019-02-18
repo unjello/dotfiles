27e32cb3569d8cbd6e33e09739ad88e29a07105c
Andrzej Lichnerowicz <andrzej@lichnerowicz.pl>
2019-02-18 11:24:34 +0100

    install: add brave browser

--- install_linux.sh
+++ install_linux.sh
@@ -147,3 +147,9 @@ code --install-extension robertohuertasm.vscode-icons
 code --install-extension rust-lang.rust
 code --install-extension samuelcolvin.jinjahtml
 
+# Install Brave Browser
+curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add 
+source /etc/os-release
+echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ $UBUNTU_CODENAME main" | sudo tee /etc/apt/sources.list.d/brave-browser-release-${UBUNTU_CODENAME}.list
+sudo apt update
+sudo apt install -y brave-browser brave-keyring
