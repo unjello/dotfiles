c2a774559b785f3b37e3c1729ca33b9b2aabe005
Andrzej Lichnerowicz <andrzej@lichnerowicz.pl>
2019-03-28 02:47:39 -0500

    deploy: add eclim workspace directory creation

--- deploy/tasks/30-eclipse.sh
+++ deploy/tasks/30-eclipse.sh
@@ -50,6 +50,8 @@ if [[ ! -f /opt/eclipse/eclimd ]]; then
   else
     sh $eclim_tmp --eclipse=/opt/eclipse --plugins=jdt --vimfiles=skip --yes
     ln -s $(dirname $(readlink -f /opt/eclipse/eclimd))/eclim /opt/eclipse/eclim
+    # workspace directory will be required by tools using eclim
+    mkdir -p ~/.workspace
     echo "Eclim successfully installed."
   fi
   popd >/dev/null && rm -rf ~/temp_deploy
