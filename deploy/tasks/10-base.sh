cbfcb1c1725f2a4655971045a6cec21dd3828ad0
Andrzej Lichnerowicz <andrzej@lichnerowicz.pl>
2019-04-06 10:53:19 +0200

    deploy: add git bash helpers

--- deploy/tasks/10-base.sh
+++ deploy/tasks/10-base.sh
@@ -4,3 +4,8 @@
 if [[ $(getOsFamily) = "rhel"]]; then
     sudo yum install -y https://centos7.iuscommunity.org/ius-release.rpm
 fi
+
+# Install git prompt helpers
+mkdir -p ~/bin
+curl -#SL https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o ~/bin/.git-completion.bash
+curl -#SL https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh -o ~/bin/.git-prompt.bash
--- install_linux.sh
+++ install_linux.sh
@@ -15,10 +15,6 @@ sudo update-alternatives --config editor
 # Install [powerline-shell](https://github.com/powerline/powerline)
 pip3 install --user powerline-status
 
-# Install git prompt helpers
-mkdir -p ~/bin
-curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash > ~/bin/.git-completion.bash
-curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh > ~/bin/.git-prompt.bash
 
 # Install kitty terminal (needs latest freetype engine)
 sudo add-apt-repository ppa:glasen/freetype2 -y
