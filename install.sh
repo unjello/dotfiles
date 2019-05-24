a9826da6c65a6c6f7a123f746105b53d92398bc0
Andrzej Lichnerowicz <andrzej@lichnerowicz.pl>
2019-05-24 14:56:50 -0500

    deploy: create yet another version of dotfiles bootstrap script

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+function get_os_family {
+  if [ -f /etc/os-release ]; then
+    # freedesktop.org and systemd
+    . /etc/os-release
+    echo $(echo $ID_LIKE | cut -f1 -d' ' | tr '[:upper:]' '[:lower:]')
+  else
+    # Fall back to uname, e.g. "Linux <version>", also works for BSD, etc.
+    echo $(uname -s)
+  fi
+}
+
+os=$(get_os_family)
+if [[ "$os" != "elemenary" ]]; then
+  echo "Elementary required"
+fi
+
+sudo apt install python-pip
+pip install virtualenvwrapper
+export WORKON_HOME=~/.virtual-envs
+mkdir -p $WORKON_HOME
+source /usr/local/bin/virtualenvwrapper.sh
